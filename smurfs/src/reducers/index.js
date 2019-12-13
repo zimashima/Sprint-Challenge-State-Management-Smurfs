5286f4aede53126a2eb28c5e1c9dde429937b5b6
Mashima <52222646+zimashima@users.noreply.github.com>
2019-12-13 11:19:05 -0500

    created initialState, reducers, actions, and store

---
+++ smurfs/src/reducers/index.js
@@ -0,0 +1,15 @@
+export const initialState = {
+    smurfs: [],
+    isFetching: false,
+    isUpdating: false,
+    isDeleting: false
+}
+
+export const smurfsReducers = (state = initialState, action) => {
+    switch(action.type){
+
+        default:
+            return state
+    }
+    
+}
